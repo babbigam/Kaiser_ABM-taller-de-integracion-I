61ec3eb69b30864bc0985b546de7b9b3cd22cf03
DburgosA <111473775+DburgosA@users.noreply.github.com>
2022-10-17 22:39:12 -0300

    tabla valores de mysql y cambios a style

--- ADatospersonales.php
+++ ADatospersonales.php
@@ -2,43 +2,98 @@
   require 'assets/js/sessionstart.php'; 
 ?>
 
+<?php
+    require 'localhost/ConBDPersonal.php';
+    $query = "SELECT * FROM `datos_personales`";
+    $result = mysqli_query($conn, $query);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="assets/css/stylemenu.css"> 
-    <link rel="stylesheet" href="assets/css/styles.css"> 
+    <link rel="stylesheet" href="assets/css/style.css"> 
     <title>Datos personales</title>
 </head>
 <body id="contenedor">
+    <!--MENU-->
     <div> <?php require "assets/js/menuaside.php" ?></div>
-    <!--Menu-->
-    <nav id="navegador">
-        <h3 class="logo">Logo</h3>
-        <h3 class="menu">Menu</h3>
-        <h3 class="usuario">Usuario</h3>
-    </nav>
     <header id="cabeza">
-        <!--Encabezado-->
-        <h2 class="imagen">imagen</h2>         
-        <h2 class="nombre">Datos personales 1</h1>
     </header>
-    <main id="cuerpo">
-        <!--Principal-->
-        <h1 class="informacion-c">Informacion sobre la compañia</h1>
-        <h1 class="m1">Marca 1</h1>
-        <h1 class="m2">Marca 2</h1>
-        <h2 class="informacion-r">Informacion y Requisitos</h2>
-        <h2 class="formulario">Formulario de Datos del Usuario</h2>
-    </main>
+
+    <div class="botonesbusqueda">
+
+    <input type="checkbox" class="checkoption" value="1" onclick="checkedOnClick(this);"> Option1
+    <input type="checkbox" class="checkoption" value="2" onclick="checkedOnClick(this);"> Option2 <br>
+    <input type="checkbox" class="checkoption" value="3" onclick="checkedOnClick(this);"> Option3
+    <input type="checkbox" class="checkoption" value="4" onclick="checkedOnClick(this);"> Option4 <br>
+    <input type="checkbox" class="checkoption" value="5" onclick="checkedOnClick(this);"> Option5
+    <!-- Script -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function(){
+    $('.checkoption').click(function() {
+        $('.checkoption').not(this).prop('checked', false);
+    });
+    });
+    </script>    
+    </div>
+
+    <div class="Busqueda">
+
+        <form action="" method= "get">
+            <p>RUT</p><input type="text" name="busquedarut"><br>
+            <input type="submit" name="enviar" value="Buscar">
+        </form>
+
+    </div>
+    <div class="tabla">
+        <?php
+        if(mysqli_num_rows($result) > 0)
+        {
+        $table = '
+        <table border=1>      									
+            <tr>
+                <th>RUT</th>
+                <th>Nombres</th>
+                <th>Apellido_paterno </th>
+                <th>Apellido_materno </th>
+                <th>Email</th>
+                <th>Nacionalidad </th>
+                <th>Domicilio </th>
+                <th>Ciudad</th>
+                <th>Comuna</th>
+                <th>Celular</th>
+                <th>Estado_civil </th>
+                <th>Fecha_nacimiento </th>
+            </tr>';
+        while($row = mysqli_fetch_array($result))
+        {
+        $table .= '
+            <tr>
+                <td>'.$row["RUT"].'</td>
+                <td>'.$row["Nombres"].'</td>
+                <td>'.$row["Apellido_paterno"].'</td>
+                <td>'.$row["Apellido_materno"].'</td>
+                <td>'.$row["Email"].'</td>
+                <td>'.$row["Nacionalidad"].'</td>
+                <td>'.$row["Domicilio"].'</td>
+                <td>'.$row["Ciudad"].'</td>
+                <td>'.$row["Comuna"].'</td>
+                <td>'.$row["Celular"].'</td>
+                <td>'.$row["Estado_civil"].'</td>
+                <td>'.$row["Fecha_nacimiento"].'</td>
+            </tr>';}
+        $table .= '</table>';
+        echo $table;
+        }
+    ?>
+    
+    </div>
+
     <footer id="pie">
-        <!--Pie de página-->
-        <h3 class="conocer">Conocenos</h3>
-        <h3 class="Pol-pri">Politica de Privacidad</h3>
-        <h3 class="direccion">Direccion</h3>
-        <h3 class="correo">Correo</h3>
-        <h3 class="ter-con">Termino y Condiciones</h3>
-        <h3 class="sucursales">Sucursales</h3>
     </footer>
+
 </body>
 </html>
--- localhost/ConBDPersonal.php
+++ localhost/ConBDPersonal.php
@@ -1,11 +1,6 @@
 <?php 
-    $server = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'kaiserabmpersonal';
-/*comprobar coneccion usando try */
     try {
-        $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);  /*la variable $con es donde se almacena la base de datos */
+        $conn = mysqli_connect("localhost", "root", "", "kaiserabmpersonal");  /*la variable $con es donde se almacena la base de datos */
       } catch (PDOException $e) {
         die('Connection Failed: ' . $e->getMessage()); /*finaliza el proceso y muestra el error */
       }
