e383c7ebd3614218917675d71f0cf1175683274f
DburgosA <111473775+DburgosA@users.noreply.github.com>
2022-10-19 16:04:59 -0300

    tabla
    tabla funcional

--- ADatospersonales.php
+++ ADatospersonales.php
@@ -13,19 +13,42 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="assets/css/stylemenu.css"> 
-    <link rel="stylesheet" href="assets/css/style2.css"> 
+    <link rel="stylesheet" href="assets/css/style.css"> 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,-25" />
     <title>Datos personales</title>
 </head>
 <body>
-    <div> <?php require "assets/js/menuaside.php" ?></div>
-        <!--HEADER-->
-        <header>
-
-        </header>
-
+        <div> <?php require "assets/js/menuaside.php" ?></div>
         <!--MAIN CONTENT-->
         <div class="main-content">
 
+            <!--HEADER-->
+            <header>
+            </header>
+        <div class="botonesbusqueda">
+            <input type="checkbox" class="checkoption" value="1" onclick="checkedOnClick(this);"> <span>RUT                </span>          
+            <input type="checkbox" class="checkoption" value="2" onclick="checkedOnClick(this);"> <span>Estado Civil       </span><br>
+            <input type="checkbox" class="checkoption" value="3" onclick="checkedOnClick(this);"> <span>Nacionalidad       </span>
+            <input type="checkbox" class="checkoption" value="4" onclick="checkedOnClick(this);"> <span>Fecha de nacimiento</span><br>
+            <input type="checkbox" class="checkoption" value="5" onclick="checkedOnClick(this);"> <span>Apellido paterno   </span>
+            <input type="checkbox" class="checkoption" value="6" onclick="checkedOnClick(this);"> <span>Comuna             </span><br>
+        <!-- Script -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+            <script type="text/javascript">
+                $(document).ready(function(){
+                $('.checkoption').click(function() {
+                    $('.checkoption').not(this).prop('checked', false);
+                });
+                });
+            </script>    
+        </div>
+
+    <div class="Busqueda">
+        <form action="" method= "get">
+            <p>ingrese el RUT:</p><input type="text" name="busquedarut">
+            <input type="submit" name="enviar" value="Buscar">
+        </form>
+    </div>
             <!--TABLE-->
             <div class="contenedor-datatable">
 
@@ -36,32 +59,21 @@
                     <div class="tools">
                         <ul>
                             <li>
-                                <span>
-                                    <input type="checkbox">
-                                </span>
-                            </li>
-                            <li>
                                 <button>
-                                    <span class="material-symbols-outlined"> add_circle </span>
+                                    <span class="material-symbols-outlined"> Añadir </span>
                                 </button>
                                 
                             </li>
                             <li>
                                 <button>
-                                    <span class="material-symbols-outlined">edit </span>      
+                                    <span class="material-symbols-outlined"> editar </span>      
                                 </button>                          
                             </li>
                             <li>
                                 <button>
-                                    <span class="material-symbols-outlined"> delete </span>   
+                                    <span class="material-symbols-outlined"> borrar </span>   
                                 </button>    
-                            </li>
-                            <li>
-                                <button>
-                                    <span class="material-symbols-outlined"> share </span>
-                                </button>
-                                
-                            </li>
+                            </li>>
                         </ul>
                     </div>
 
@@ -76,45 +88,45 @@
                 <table class="datatable">
                     <thead>
                         <?php
-        if(mysqli_num_rows($result) > 0)
-        {
-        $table = '
-        <table border=1>      									
-            <tr>
-                <th>RUT</th>
-                <th>Nombres</th>
-                <th>Apellido_paterno </th>
-                <th>Apellido_materno </th>
-                <th>Email</th>
-                <th>Nacionalidad </th>
-                <th>Domicilio </th>
-                <th>Ciudad</th>
-                <th>Comuna</th>
-                <th>Celular</th>
-                <th>Estado_civil </th>
-                <th>Fecha_nacimiento </th>
-            </tr>';
-        while($row = mysqli_fetch_array($result))
-        {
-        $table .= '
-            <tr>
-                <td>'.$row["RUT"].'</td>
-                <td>'.$row["Nombres"].'</td>
-                <td>'.$row["Apellido_paterno"].'</td>
-                <td>'.$row["Apellido_materno"].'</td>
-                <td>'.$row["Email"].'</td>
-                <td>'.$row["Nacionalidad"].'</td>
-                <td>'.$row["Domicilio"].'</td>
-                <td>'.$row["Ciudad"].'</td>
-                <td>'.$row["Comuna"].'</td>
-                <td>'.$row["Celular"].'</td>
-                <td>'.$row["Estado_civil"].'</td>
-                <td>'.$row["Fecha_nacimiento"].'</td>
-            </tr>';}
-        $table .= '</table>';
-        echo $table;
-        }
-    ?>
+                            if(mysqli_num_rows($result) > 0)
+                            {
+                            $table = '
+                            <table border=1>      									
+                                <tr>
+                                    <th>RUT</th>
+                                    <th>Nombres</th>
+                                    <th>Apellido_paterno </th>
+                                    <th>Apellido_materno </th>
+                                    <th>Email</th>
+                                    <th>Nacionalidad </th>
+                                    <th>Domicilio </th>
+                                    <th>Ciudad</th>
+                                    <th>Comuna</th>
+                                    <th>Celular</th>
+                                    <th>Estado_civil </th>
+                                    <th>Fecha_nacimiento </th>
+                                </tr>';
+                            while($row = mysqli_fetch_array($result))
+                            {
+                            $table .= '
+                                <tr>
+                                    <td>'.$row["RUT"].'</td>
+                                    <td>'.$row["Nombres"].'</td>
+                                    <td>'.$row["Apellido_paterno"].'</td>
+                                    <td>'.$row["Apellido_materno"].'</td>
+                                    <td>'.$row["Email"].'</td>
+                                    <td>'.$row["Nacionalidad"].'</td>
+                                    <td>'.$row["Domicilio"].'</td>
+                                    <td>'.$row["Ciudad"].'</td>
+                                    <td>'.$row["Comuna"].'</td>
+                                    <td>'.$row["Celular"].'</td>
+                                    <td>'.$row["Estado_civil"].'</td>
+                                    <td>'.$row["Fecha_nacimiento"].'</td>
+                                </tr>';}
+                            $table .= '</table>';
+                            echo $table;
+                            }
+                        ?>
                     </thead>
 
                     
