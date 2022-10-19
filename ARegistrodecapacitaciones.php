46d1dbbee4d3029ef269389b12d472382e0d350d
DburgosA <111473775+DburgosA@users.noreply.github.com>
2022-10-19 17:04:54 -0300

    ahora si los modulos

--- ARegistrodecapacitaciones.php
+++ ARegistrodecapacitaciones.php
@@ -7,7 +7,8 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="assets/css/stylemenu.css"> 
-    <link rel="stylesheet" href="assets/css/styles.css"> 
+    <link rel="stylesheet" href="assets/css/style.css"> 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,-25" />
     <title>Registro de capacitaciones</title>
 </head>
 <body>
--- ARegistrodelicencias.php
+++ ARegistrodelicencias.php
@@ -7,9 +7,10 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="assets/css/stylemenu.css"> 
-    <link rel="stylesheet" href="assets/css/styles.css"> 
+    <link rel="stylesheet" href="assets/css/style.css"> 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,-25" />
     <title>Registro de licencias</title>
-    </head>
+</head>
 <body>
     <div> <?php require "assets/js/menuaside.php" ?></div>
     <!--MAIN CONTENT-->
