6774e62aec924cafc0515e306f382f3ee070e623
Katterinne Sandoval <kattitami@gmail.com>
2022-11-03 00:35:40 -0300

    maqueteo capacitacion, desvinculados, logousuario

---
+++ mod_capacitaciones.php
@@ -0,0 +1,88 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="capacitaciones_style.css">
+    <title>Capacitaciones</title>
+</head>
+<body>
+    <main>
+        <div class="menu">
+            <ul>
+                <li><img src="Logousuario.png" alt=""></li>
+                <li>Registro de usuario</li>
+                <li>Datos personales</li>
+                <li>Vacaciones</li>
+                <li>Regislio de licencias</li>
+                <li>Historial</li>
+                <li>Registro de capacitaciones</li>
+            </ul>
+        </div>
+        <div class="filtro">
+            <ul>
+                <li>Boton Tema</li>
+                <li>Label Tema</li>
+                <li>Boton Fecha</li>
+                <li>Label fecha</li>
+                <li>Boton Expositor/a</li>
+                <li>Label Expositor/a</li>
+                <li>Boton Duracion</li>
+                <li>Label Duracion</li>
+                <li>Boton Lugar</li>
+                <li>Label Lugar</li>
+                <li>Boton Cargo_oyente</li>
+                <li>Label Cargo_oyente</li>
+                <li>Boton Asistencia</li>
+                <li>Label Asistencia</li>
+            </ul>
+        </div>
+        <div class="tabla-de-datos">
+            <table>
+                <tr>
+                    <td>Tema</td>
+                    <td>Fecha</td>
+                    <td>Expositor/a</td>
+                    <td>Duracion</td>
+                    <td>Lugar</td>
+                    <td>Cargo_oyente</td>
+                    <td>Asistencia</td>
+                </tr>
+                <tr>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                </tr>
+                <tr>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                </tr>
+                
+            </table>
+        </div>
+        <div class="descarga">
+            <table>
+                <caption>Descarga de archivos</caption>
+                <tr>
+                    <td>Boton PDF</td>
+                    <td>Boton Excel</td>
+                </tr>
+            </table>
+        </div>
+    </main>
+</body>
+</html>
---
+++ mod_desvinculacion.php
@@ -0,0 +1,83 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="desvinvculacion_style.css">
+    <title>desvinculados</title>
+</head>
+<body>
+    <main>
+        <div class="menu">
+            <ul>
+                <li><img src="Logousuario.png" alt=""></li>
+                <li>Registro de usuario</li>
+                <li>Datos personales</li>
+                <li>Vacaciones</li>
+                <li>Regislio de licencias</li>
+                <li>Historial</li>
+                <li>Registro de capacitaciones</li>
+            </ul>
+        </div>
+        <div class="filtro">
+            <ul>
+                <li>Boton Nombres</li>
+                <li>Label Nombres</li>
+                <li>Boton Apellido paterno</li>
+                <li>Label Apellido paterno</li>
+                <li>Boton Apellido materno</li>
+                <li>Label Apellido materno</li>
+                <li>Boton Cargo</li>
+                <li>Label Cargo</li>
+                <li>Boton Local</li>
+                <li>Label Local</li>
+                <li>Boton Razon</li>
+                <li>Label Razon</li>
+            </ul>
+        </div>
+        <div class="tabla-de-datos">
+            <table>
+                <tr>
+                    <td>Nombres</td>
+                    <td>Apellido paterno</td>
+                    <td>Apellido materno</td>
+                    <td>Cargo</td>
+                    <td>Local</td>
+                    <td>Razon</td>
+                </tr>
+                <tr>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                </tr>
+                <tr>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                    <td>datos</td>
+                </tr>
+                
+            </table>
+        </div>
+        <div class="descarga">
+            <table>
+                <caption>Descarga de archivos</caption>
+                <tr>
+                    <td>Boton PDF</td>
+                    <td>Boton Excel</td>
+                </tr>
+            </table>
+        </div>
+    </main>
+</body>
+</html>
