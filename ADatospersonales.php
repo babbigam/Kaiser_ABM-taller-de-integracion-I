df073647f0360e5613095003c32020a8f4499eee
DburgosA <111473775+DburgosA@users.noreply.github.com>
2022-11-16 00:42:05 -0300

    funcion buscar por rut

--- ADatospersonales.php
+++ ADatospersonales.php
@@ -1,6 +1,20 @@
 <?php
   require 'assets/js/sessionstart.php'; 
 ?>
+<?php
+    require 'localhost/ConBDPersonal.php';
+        $where = "";
+        if (!empty($_POST)){
+            $valor = $_POST["busqueda1"];
+
+            if(!empty($valor)){
+                $where = "WHERE RUT LIKE $valor";
+            }
+        }
+        $query = "SELECT * FROM datos_personales $where";
+        $result = mysqli_query($conn, $query);
+    require 'localhost/closeBDPersonal.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -26,101 +40,85 @@
                     <input type="checkbox" class="checkoption" id="5" onclick="checkedOnClick(this);"><span>Apellido paterno    </span><br>
                     <input type="checkbox" class="checkoption" id="6" onclick="checkedOnClick(this);"><span>Comuna              </span><br>
                 </div>
-
-                        <div class="Busqueda">
-                            <div class="busquedarut" style="display: none">
-                                <form action="" method= "get">
-                                <p>ingrese el RUT:</p><input type="text" name="busqueda1">
-                                <input type="submit" name="enviar1" value="Buscar">
-                            </form>
-                        </div>
-
-                        <div class="busquedarestado" style="display: none">
-                                <form action="" method= "get">
-                                    <p>ingrese el Estado Civil:</p><input type="text" name="busqueda2">
-                                    <input type="submit" name="enviar2" value="Buscar">
-                            </form>
-                        </div>
-                        <div class="busquedanacionalidad" style="display: none">
-                            <form action="" method= "get">
-                                <p>ingrese la nacionalidad:</p><input type="text" name="busqueda3">
-                                <input type="submit" name="enviar3" value="Buscar">
-                            </form>
-                        </div>
-                        <div class="busquedafechanaci" style="display: none">
-                            <form action="" method= "get">
-                                <p>ingrese la fecha de nacimiento:</p><input type="text" name="busqueda4">
-                                <input type="submit" name="enviar4" value="Buscar">
-                            </form>
-                        </div>
-                        <div class="busquedaapellidopaterno" style="display: none">
-                            <form action="" method= "get">
-                                <p>ingrese el apellido paterno:</p><input type="text" name="busqueda5">
-                                <input type="submit" name="enviar5" value="Buscar">
-                            </form>
-                        </div>
-                        <div class="busquedacomuna" style="display: none">
-                            <form action="" method= "get">
-                                <p>ingrese la comuna:</p><input type="text" name="busqueda6">
-                                <input type="submit" name="enviar6" value="Buscar">
-                            </form>
-                        </div>
+                <!--BUSQUEDA-->
+                <div class="Busqueda">
+                    <div class="busquedarut" style="display: none">
+                        <form action="" method= "post" >
+                            <p>ingrese el RUT:</p> <input type="text" name="busqueda1">
+                            <input type="submit" name="enviar1" value="Buscar">
+                        </form>
                     </div>
+                    <div class="busquedarestado" style="display: none">
+                        <form action="" method= "post" >
+                            <p>ingrese Estado_civil:</p> <input type="text" name="busqueda2">
+                            <input type="submit" name="enviar2" value="Buscar">
+                        </form>
+                    </div>
+                    <div class="busquedanacionalidad" style="display: none">
+                        <form action="" method= "post" >
+                            <p>ingrese la nacionalidad:</p> <input type="text" name="busqueda3">
+                            <input type="submit" name="enviar3" value="Buscar">
+                        </form>
+                    </div>
+                    <div class="busquedafechanaci" style="display: none">
+                        <form action="" method= "post" >
+                            <p>ingrese la fecha nacimiento:</p> <input type="text" name="busqueda4">
+                            <input type="submit" name="enviar4" value="Buscar">
+                        </form>
+                    </div>
+                    <div class="busquedaapellidopaterno" style="display: none">
+                        <form action="" method= "post" >
+                            <p>ingrese el apellido paterno:</p> <input type="text" name="busqueda5">
+                            <input type="submit" name="enviar5" value="Buscar">
+                        </form>
+                    </div>
+                    <div class="busquedacomuna" style="display: none">
+                        <form action="" method= "post" >
+                            <p>ingrese la comuna:</p> <input type="text" name="busqueda6">
+                            <input type="submit" name="enviar6" value="Buscar">
+                        </form>
+                    </div>
+                    <!---->
+                </div>
+                <br>
+                <div class="tabla-cont">
+                    <table class="tabla">
+                        <thead class="text-muted">
+                            <th>RUT</th>
+                            <th>Nombres</th>
+                            <th>Apellido_paterno </th>
+                            <th>Apellido_materno </th>
+                            <th>Email</th>
+                            <th>Nacionalidad </th>
+                            <th>Domicilio </th>
+                            <th>Ciudad</th>
+                            <th>Comuna</th>
+                            <th>Celular</th>
+                            <th>Estado_civil </th>
+                            <th>Fecha_nacimiento </th>
+                        </thead>
+                        <tbody>
+                            <?php while($row = $result->fetch_assoc()){?>
+                            <tr>
+                                <td><?php echo $row["RUT"]?></td>
+                                <td><?php echo $row["Nombres"]?></td>
+                                <td><?php echo $row["Apellido_paterno"]?></td>
+                                <td><?php echo $row["Apellido_materno"]?></td>
+                                <td><?php echo $row["Email"]?></td>
+                                <td><?php echo $row["Nacionalidad"]?></td>
+                                <td><?php echo $row["Domicilio"]?></td>
+                                <td><?php echo $row["Ciudad"]?></td>
+                                <td><?php echo $row["Comuna"]?></td>
+                                <td><?php echo $row["Celular"]?></td>
+                                <td><?php echo $row["Estado_civil"]?></td>
+                                <td><?php echo $row["Fecha_nacimiento"]?></td>
+                            </tr>
+                        <?php }?>
+
+                        </tbody>
+                    </table>
                 </div>
                 <!--DATA TABLE-->
-                <table class="datatable">
-
-                    <?php
-                        require 'localhost/ConBDPersonal.php';
-                            $query = "SELECT * FROM `datos_personales`";
-                            $result = mysqli_query($conn, $query);
-                        require 'localhost/closeBDPersonal.php';
-                    ?>
-
-                    <thead>
-                        <?php
-                            if(mysqli_num_rows($result) > 0)
-                            {
-                            $table = '
-                            <table border=1>      									
-                                <tr>
-                                    <th>RUT</th>
-                                    <th>Nombres</th>
-                                    <th>Apellido_paterno </th>
-                                    <th>Apellido_materno </th>
-                                    <th>Email</th>
-                                    <th>Nacionalidad </th>
-                                    <th>Domicilio </th>
-                                    <th>Ciudad</th>
-                                    <th>Comuna</th>
-                                    <th>Celular</th>
-                                    <th>Estado_civil </th>
-                                    <th>Fecha_nacimiento </th>
-                                </tr>';
-                            while($row = mysqli_fetch_array($result))
-                            {
-                            $table .= '
-                                <tr>
-                                    <td>'.$row["RUT"].'</td>
-                                    <td>'.$row["Nombres"].'</td>
-                                    <td>'.$row["Apellido_paterno"].'</td>
-                                    <td>'.$row["Apellido_materno"].'</td>
-                                    <td>'.$row["Email"].'</td>
-                                    <td>'.$row["Nacionalidad"].'</td>
-                                    <td>'.$row["Domicilio"].'</td>
-                                    <td>'.$row["Ciudad"].'</td>
-                                    <td>'.$row["Comuna"].'</td>
-                                    <td>'.$row["Celular"].'</td>
-                                    <td>'.$row["Estado_civil"].'</td>
-                                    <td>'.$row["Fecha_nacimiento"].'</td>
-                                </tr>';}
-                            $table .= '</table>';
-                            echo $table;
-                            }
-                        ?>
-                    </thead>
-                </table>
-
                 <!--TABLE FOOTER ELEMENTS-->
                 <div class="datatable-footer">
                     <div class="items-visibles">
@@ -132,43 +130,14 @@
                         </select>
                         Personas
                     </div>
-
-                    <div class="pages">
-                        <ul>
-                            <li>
-                                <button>
-                                    <span class="active">1</span>
-                                </button>
-                            </li>
-                            <li>
-                                <button>2</button>
-                            </li>
-                            <li>
-                                <button>3</button>
-                            </li>
-                            <li>
-                                <button>4</button>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
-            </div>
-
-            <div class="filters">
-
-            </div>
-
-            <div class="footer">
-
-            </div>
-        </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-            <script type="text/javascript">
-                $(document).ready(function(){
-                    $('.checkoption').click(function() {
-                    $('.checkoption').not(this).prop('checked', false);
-                    });
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('.checkoption').click(function() {
+                $('.checkoption').not(this).prop('checked', false);
                 });
+            });
         </script>    
         <script>
             $(document).ready(function(){
